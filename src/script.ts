2beaf4c4ef9c414b38a29e905c8a42b5ad19aca5
noClaps <github@zerolimits.dev>
2024-01-06 23:56:49 +0000

    Add command history
    This was so much easier to implement than I thought it would be.

--- src/script.ts
+++ src/script.ts
@@ -4,6 +4,9 @@ import help from "./bin/help";
 import about from "./bin/about";
 import ls from "./bin/ls";
 
+const history: string[] = [];
+let currentPos = history.length - 1;
+
 const input = document.querySelector("input")!;
 const log = document.getElementById("log")!;
 
@@ -13,8 +16,28 @@ log.innerHTML += `Type "help" to see all available commands.\n`;
 document.addEventListener("click", () => input.focus());
 
 input.onkeyup = (e) => {
+    if (e.key === "ArrowUp") {
+        e.preventDefault();
+        if (currentPos >= 0) {
+            input.value = history[currentPos];
+            currentPos -= 1;
+        }
+    }
+
+    if (e.key === "ArrowDown") {
+        e.preventDefault();
+        if (currentPos < history.length - 1) {
+            currentPos += 1;
+            input.value = history[currentPos];
+        } else {
+            input.value = "";
+        }
+    }
+
     if (e.key === "Enter") {
         const cmd = input.value.toLowerCase().trim();
+        history.push(cmd);
+        currentPos = history.length - 1;
 
         if (cmd === "clear") {
             log.innerHTML = "";
