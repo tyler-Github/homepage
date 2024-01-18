480ce19905e8fe0de6b6cea890f873eb8717f1cd
noClaps <github@zerolimits.dev>
2024-01-18 08:36:45 +0000

    Remove themes
    It's too much effort to maintain and I don't really feel like it

--- src/bin/ls.ts
+++ src/bin/ls.ts
@@ -4,7 +4,6 @@ OPTIONS
     projects        ZeroLimits.dev - <a href="https://zerolimits.dev">Link</a> - <a href="https://github.com/noClaps/ZeroLimits.dev">Source</a>
                     The Blog of Random - <a href="https://blog.zerolimits.dev">Link</a> - <a href="https://github.com/noClaps/blog">Source</a>
                     Aperturic Focus - <a href="https://gallery.zerolimits.dev">Link</a> - <a href="https://github.com/noClaps/gallery">Source</a>
-                    Themes - <a href="https://github.com/noClaps/themes">Source</a>
     contact         Email - <a href="mailto:contact@zerolimits.dev">contact@zerolimits.dev</a>
 `;
     const args: { [index: string]: string; } = {
@@ -12,7 +11,6 @@ OPTIONS
 ZeroLimits.dev - <a href="https://zerolimits.dev">Link</a> - <a href="https://github.com/noClaps/ZeroLimits.dev">Source</a>
 The Blog of Random - <a href="https://blog.zerolimits.dev">Link</a> - <a href="https://github.com/noClaps/blog">Source</a>
 Aperturic Focus - <a href="https://gallery.zerolimits.dev">Link</a> - <a href="https://github.com/noClaps/gallery">Source</a>
-Themes - <a href="https://github.com/noClaps/themes">Source</a>
 `,
         contact: `
 Email - <a href="mailto:contact@zerolimits.dev">contact@zerolimits.dev</a>
