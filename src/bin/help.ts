e2cc3e0294c840fec42e96904bb8e789f6529fb5
noClaps <github@zerolimits.dev>
2024-01-31 01:33:25 +0000

    Add random number generator project

--- src/bin/help.ts
+++ src/bin/help.ts
@@ -2,17 +2,17 @@ export default function help(arg: string | undefined) {
     const html =
         `
 COMMANDS
-    help        Display this help menu
-    clear       Clear the screen
-    about       Show details about the site
-    ls          List sections
+  help         Display this help menu
+  clear        Clear the screen
+  about        Show details about the site
+  ls           List sections
 `;
 
     const args: { [index: string]: string; } = {
-        help: `help        Display this help menu`,
-        clear: `clear       Clear the screen`,
-        about: `about       Show details about the site`,
-        ls: `ls          List sections`
+        help: `help           Display this help menu`,
+        clear: `clear          Clear the screen`,
+        about: `about          Show details about the site`,
+        ls: `ls             List sections`
     };
 
     if (arg) {
